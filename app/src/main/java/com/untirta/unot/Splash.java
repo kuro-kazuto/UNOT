3de540cf481ee800ba9a6be13c667f1a884d0e4f
kuro-kazuto <galihajipambudi@gmail.com>
2021-09-09 18:38:12 +0700

    9 sept 2021

---
+++ app/src/main/java/com/untirta/unot/Splash.java
@@ -0,0 +1,35 @@
+//AKTIVITY SPLASH SCREEN
+package com.untirta.unot;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        setContentView(R.layout.activity_splash);
+        int waktu_loading = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent login=new Intent(Splash.this, Dashboard.class);
+                startActivity(login);
+                finish();
+
+            }
+        }, waktu_loading);
+    }
+}
