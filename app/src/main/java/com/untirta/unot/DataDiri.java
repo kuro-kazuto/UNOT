9d901e1671ccfef816cfa25b6902c36a9f286c05
kuro-kazuto <galihajipambudi@gmail.com>
2021-10-10 10:30:05 +0700

    10/10/21 *MAJOR - mengubah layout menjadi lebih efisien dengan dialog - mengubah nama class dan activity dan menyatukan activity
    *MINOR
    -mengubah tata letak layout dan optimalisasi tampilan

--- app/src/main/java/com/untirta/unot/DataDiri.java
+++ app/src/main/java/com/untirta/unot/DataDiri.java
@@ -10,88 +10,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class DataDiri extends AppCompatActivity {
-    Button btnTE, btnM, btnK, btnI, btnMetal, btnS, dj;
+    Button dj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datadiri);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        dj = findViewById(R.id.dialogjurusan);
-        btnI = findViewById(R.id.TI);
-        btnK = findViewById(R.id.TK);
-        btnM = findViewById(R.id.TM);
-        btnMetal = findViewById(R.id.TMetal);
-        btnTE = findViewById(R.id.TE);
-        btnS = findViewById(R.id.TS);
 
         String NIM = getIntent().getStringExtra("name");
-
-        btnI.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String jurusan = "Teknik Industri";
-                Intent intent =new Intent(DataDiri.this, AdminActivity.class);
-                intent.putExtra("name", NIM);
-                intent.putExtra("pilihan", jurusan);
-                startActivity(intent);
-            }
-        });
-
-        btnK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String jurusan = "Teknik Kimia";
-                Intent intent =new Intent(DataDiri.this, AdminActivity.class);
-                intent.putExtra("name", NIM);
-                intent.putExtra("pilihan", jurusan);
-                startActivity(intent);
-            }
-        });
-
-        btnM.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String jurusan = "Teknik Mesin";
-                Intent intent =new Intent(DataDiri.this, AdminActivity.class);
-                intent.putExtra("name", NIM);
-                intent.putExtra("pilihan", jurusan);
-                startActivity(intent);
-            }
-        });
-
-        btnMetal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String jurusan = "Teknik Metalurgi";
-                Intent intent =new Intent(DataDiri.this, AdminActivity.class);
-                intent.putExtra("name", NIM);
-                intent.putExtra("pilihan", jurusan);
-                startActivity(intent);
-            }
-        });
-
-        btnTE.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String jurusan = "Teknik Elektro";
-                Intent intent =new Intent(DataDiri.this, AdminActivity.class);
-                intent.putExtra("name", NIM);
-                intent.putExtra("pilihan", jurusan);
-                startActivity(intent);
-            }
-        });
-
-        btnS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String jurusan = "Teknik Sipil";
-                Intent intent =new Intent(DataDiri.this, AdminActivity.class);
-                intent.putExtra("name", NIM);
-                intent.putExtra("pilihan", jurusan);
-                startActivity(intent);
-            }
-        });
 
         dj.setOnClickListener(new View.OnClickListener() {
             @Override
